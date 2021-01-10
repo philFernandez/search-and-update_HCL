b14e8d02fed1f516e2d5800e1add642dd0a14687
Phil Fernandez <norcal618@gmail.com>
2021-01-09 19:31:21 -0800

    stuff

--- src/main/java/com/hcl/searchandupdate/SearchAndUpdateApplication.java
+++ src/main/java/com/hcl/searchandupdate/SearchAndUpdateApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-// @ComponentScan(basePackages = "com.hcl.searchandupdate")
 @EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class SearchAndUpdateApplication {
 
